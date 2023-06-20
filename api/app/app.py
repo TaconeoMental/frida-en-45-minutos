b86547449d32c322110021856fe097487b974b17
Mateo Contenla <25857822+TaconeoMental@users.noreply.github.com>
2023-06-19 20:35:31 -0400

    Error menor

--- api/app/app.py
+++ api/app/app.py
@@ -63,5 +63,5 @@ def handle_action():
     response = {"status": "OK"} | enc_user
     return response, 200
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
